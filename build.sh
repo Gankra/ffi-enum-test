e63200f9460296bc2bd391d25ee1f377d41f354f
Alexis Beingessner <a.beingessner@gmail.com>
2018-04-30 13:29:16 -0400

    add cargo path commented out

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 # cargo build
 rustc --crate-type=staticlib wr.rs
 g++ -c -std=c++14 main.cpp
+# g++ -o main.o target/debug/libwr.a -Wl,--gc-sections -lpthread -ldl
 g++ -o main.o libwr.a -Wl,--gc-sections -lpthread -ldl
